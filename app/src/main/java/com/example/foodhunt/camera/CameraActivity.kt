1251c3397526bb06d1845cd1af359705dc60600c
azizahfzahr <afathimatuzzahro@gmail.com>
2023-05-23 14:23:36 +0700

    Commit button galery and camera on activity camera layout

--- app/src/main/java/com/example/foodhunt/camera/CameraActivity.kt
+++ app/src/main/java/com/example/foodhunt/camera/CameraActivity.kt
@@ -2,11 +2,20 @@ package com.example.foodhunt.camera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.foodhunt.R
 
 class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+    }
+    fun sendMessage(view: View){
+        Toast.makeText(this, "Masuk ke Gallery", Toast.LENGTH_SHORT).show()
+    }
+
+    fun sendMessage2(view: View){
+        Toast.makeText(this, "Masuk ke Camera", Toast.LENGTH_SHORT).show()
     }
 }
