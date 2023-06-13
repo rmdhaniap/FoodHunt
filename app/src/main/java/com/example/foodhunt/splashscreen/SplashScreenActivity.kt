837097b34cf2f2a190e01017ec220cf3063b4d38
azizahfzahr <afathimatuzzahro@gmail.com>
2023-06-13 08:46:36 +0700

    Commit Splash Screen

---
+++ app/src/main/java/com/example/foodhunt/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.foodhunt.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.splashscreen.SplashScreen
+import com.example.foodhunt.MainActivity
+import com.example.foodhunt.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }, (3000))
+    }
+}
